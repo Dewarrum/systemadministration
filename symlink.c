621120f8b31b23546c7155d25dc73682d5e861c5
Yuri <student@mail.corp12.un>
2020-05-19 15:24:44 +0000

    Modified lab9

--- symlink.c
+++ symlink.c
@@ -1,26 +1,42 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
 
 int main() {
 	char pathname[] = "file";
 	char symlink_pathname[] = "symlink_";
 	int symlink_pathname_length = strlen(symlink_pathname);
-	char index_code[3];
+	char index_code[4];
 
 	open("symlink_0", O_RDWR);
 	symlink(pathname, "symlink_0");
 
-	for (int i = 1; i < 128; i++) {
-		itoa(i, index_code, 10);
-		char *new_pathname = strcat(symlink_pathname, index_code);
+	char *new_pathname = (char *) malloc(symlink_pathname_length + 4);
+	char *old_pathname = (char *) malloc(symlink_pathname_length + 4);
+	int i;
+	for (i = 1; i < 128; i++) {
+		// itoa(i, index_code, 10);
+		snprintf(index_code, 4, "%d", i);
+		// printf("Index code %s\n", index_code);
+
+		strcpy(new_pathname, symlink_pathname);
+		strcat(new_pathname, index_code);
 		open(new_pathname, O_RDWR);
 
 
-		itoa(i - 1, index_code, 10);
-		char *old_pathname = strcat(symlink_pathname, index_code);
-
+		// itoa(i - 1, index_code, 10);
+		snprintf(index_code, 4, "%d", i - 1);
+		strcpy(old_pathname, symlink_pathname);
+		strcat(old_pathname, index_code);
+		// printf("Iteration %d\n", i);
+		// printf("Old path: %s\n", old_pathname);
+		// printf("New path: %s\n\n", new_pathname);
 		symlink(old_pathname, new_pathname);
 	}
+
+	printf("Iteration count: %d\n", i + 1);
 
 	return 0;
 }
