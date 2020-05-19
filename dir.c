a5798a3b5dc2b922b3a8370f22062d4de7cd786f
Yuri <student@mail.corp12.un>
2020-05-19 16:05:42 +0000

    Fixed dir.c

--- dir.c
+++ dir.c
@@ -9,11 +9,15 @@
 int main(int argc, char **argv) {
 	char *path = argv[1];
 
+	if (argc < 2) {
+		path = "./";
+	}
+
 	DIR* dir = opendir(path);
 	struct dirent *currentdir;
 
 	while ((currentdir = readdir(dir)) != NULL) {
-		printf("%s", currentdir.d_name);
+		printf("%s %d \n", currentdir->d_name, currentdir->d_ino);
 	}
 
 	return 0;
