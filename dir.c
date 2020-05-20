af84475fa4b1e92a7f4057d0c210b39f06537057
Yuri <student@mail.corp12.un>
2020-05-20 15:22:55 +0000

    Fixed dir.c

--- dir.c
+++ dir.c
@@ -4,7 +4,25 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+
+void print_file_mode(mode_t mode) {
+	printf((S_ISDIR(mode)) ? "d" : "-");
+	printf((mode & S_IRUSR) ? "r" : "-");
+	printf((mode & S_IWUSR) ? "w" : "-");
+	printf((mode & S_IXUSR) ? "x" : "-");
+	printf((mode & S_IRGRP) ? "r" : "-");
+	printf((mode & S_IWGRP) ? "w" : "-");
+	printf((mode & S_IXGRP) ? "x" : "-");
+	printf((mode & S_IROTH) ? "r" : "-");
+	printf((mode & S_IWOTH) ? "w" : "-");
+	printf((mode & S_IXOTH) ? "x" : "-");
+	printf(" ");
+}
 
 int main(int argc, char **argv) {
 	char *path = argv[1];
@@ -15,9 +33,26 @@ int main(int argc, char **argv) {
 
 	DIR* dir = opendir(path);
 	struct dirent *currentdir;
+	char fullpath[256];
+	struct stat filestat;
 
 	while ((currentdir = readdir(dir)) != NULL) {
-		printf("%s %d \n", currentdir->d_name, currentdir->d_ino);
+		// strcpy(fullpath, path);
+		// printf("Before strcat, %s, %s\n", fullpath, currentdir->d_name);
+		// strcat(fullpath, currentdir->d_name);
+		snprintf(fullpath, 256, "%s%s", path, currentdir->d_name);
+		// printf("%s\n", fullpath);
+
+		if (stat(fullpath, &filestat) == -1) {
+			perror("stat");
+			exit(-1);
+		}
+
+		print_file_mode(filestat.st_mode);
+		printf("%ld %s %s %ld %.19s %s\n", filestat.st_nlink,
+			getpwuid(filestat.st_uid)->pw_name,
+			getgrgid(filestat.st_gid)->gr_name,
+			filestat.st_size, ctime(&filestat.st_mtime), currentdir->d_name);
 	}
 
 	return 0;
