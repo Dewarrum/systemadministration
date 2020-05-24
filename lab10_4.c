72dc47024333762ee0ef5a5b8c3b7d5b59a09c08
Yuri Tsai <dewarrum@gmail.com>
2020-05-24 18:35:49 +0700

    Fixed lab10_4.c

--- lab10_4.c
+++ lab10_4.c
@@ -7,8 +7,12 @@
 #include <errno.h>
 
 void my_handler(int nsig) {
-	printf("You hit %d\n", nsig);
+	printf("Receive signal %d, CTRL-C pressed\n", nsig);
 	exit(0);
+}
+
+void another_handler(int nsig) {
+	printf("Receive signal %d, CTRL-4 pressed\n", nsig);
 }
 
 int main(void) {
@@ -16,7 +20,8 @@ int main(void) {
 	pid_t pid;
 	int i;
 
-	(void) signal(SIG_DFL, my_handler);
+	(void) signal(SIGINT, my_handler);
+	(void) signal(SIGQUIT, another_handler);
 
 	while (1);
 	return 0;
